5d2883ce7aff8b0e144527538c2cf521eb3ee1fd
jiuling0 <fengzhengxia@gmail.com>
2018-06-06 00:11:33 +0800

    Decorator partial Func

--- src/lxf-python3.py
+++ src/lxf-python3.py
@@ -113,3 +113,106 @@ if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
     print('测试通过!')
 else:
     print('测试失败!')
+
+
+def is_odd(n):
+    return n % 2 == 1
+
+
+# L = list(filter(is_odd, range(1, 20)))
+L = list(filter(lambda n: n % 2 == 1, range(1, 20)))
+
+print(L)
+
+
+# decorator
+
+
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s' % func.__name__)  # 先打印日志
+        return func(*args, **kw)  # 再紧接着调用原始函数
+    
+    return wrapper
+
+
+# 要借助Python的@语法，把decorator置于函数的定义处
+# @log放到now()函数的定义处，相当于执行了语句：now = log(now)
+@log
+def now():
+    print('2015-3-25')
+
+
+now()
+
+import functools
+
+
+def log(text):
+    def decorator(func):
+        # 需要把原始函数的__name__等属性复制到wrapper()函数中，否则，有些依赖函数签名的代码执行就会出错。
+        # 不需要编写wrapper.__name__ = func.__name__这样的代码，Python内置的functools.wraps就是干这个事的
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s 33%s():' % (text, func.__name__))
+            return func(*args, **kw)
+        
+        # print('%s22 %s():' % (text, func.__name__))
+        return wrapper
+    
+    # print('%s11 %s():' % (text,'11'))
+    return decorator
+
+
+@log('excute')
+def now():
+    print('2015-3-25')
+
+
+print('a')
+now()
+
+# 请设计一个decorator，它可作用于任何函数上，并打印该函数的执行时间：
+# -*- coding: utf-8 -*-
+import time, functools
+
+
+def logger(text='excute'):
+    # @functools.wraps(text)
+    def metric(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            print('begin call %s:' % (fn.__name__))
+            t = time.time()
+            result = fn(*args, **kwargs)
+            t = time.time() - t
+            print('%s %s in %s ms' % (fn.__name__, text, t * 1000))
+            print('end call %s.' % (fn.__name__))
+            return result
+        
+        return wrapper
+    
+    return metric
+
+
+# 测试
+# @metric
+@logger('run')
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+
+# @metric
+@logger
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
