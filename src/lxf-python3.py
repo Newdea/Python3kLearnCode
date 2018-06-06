1036b7a227c209bd2fb0f4ab724f5a27506f6d3e
jiuling0 <fengzhengxia@gmail.com>
2018-06-06 22:49:22 +0800

    add partial Func

--- src/lxf-python3.py
+++ src/lxf-python3.py
@@ -216,3 +216,18 @@ if f != 33:
     print('测试失败!')
 elif s != 7986:
     print('测试失败!')
+
+
+# 偏函数
+def int2(x, base=2):
+    return int(x, base)
+
+
+int2('1010101')
+
+# functools.partial就是帮助我们创建一个偏函数的，不需要我们自己定义int2()，可以直接使用下面的代码创建一个新的函数int2
+# functools.partial的作用就是，把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单。
+int2 = functools.partial(int, base=2)
+
+# 仅仅是把base参数重新设定默认值为2，但也可以在函数调用时传入其他值：
+int2('1000000', base=10)
