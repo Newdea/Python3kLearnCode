61b9025e7068b11b086d688601e3a63745462949
Newdea <fengzhengxia@gmail.com>
2019-03-16 17:34:33 +0800

    test unit todo

--- src/test_str2float.py
+++ src/test_str2float.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
 
-
+# todo: how to make a test case.
 class TestStr2float(TestCase):
+    print('str2float(\'123.456\') =', str2float('123.456'))
+    if abs(str2float('123.456') - 123.456) < 0.00001:
+        print('测试成功!')
+    else:
+        print('测试失败!')
+
     pass
